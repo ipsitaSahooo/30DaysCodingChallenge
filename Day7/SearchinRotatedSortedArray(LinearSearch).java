80c377367799c8483da1a515af02b87245b540ce
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-04-03 00:07:57 +0530

    Update SearchinRotatedSortedArray(LinearSearch).java

--- Day7/SearchinRotatedSortedArray(LinearSearch).java
+++ Day7/SearchinRotatedSortedArray(LinearSearch).java
@@ -1,1 +1,15 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static int findPosition(ArrayList<Integer> arr, int n, int k) 
+    {
+        int ans = -1;
+        for (int i = 0; i < n; i++) {
+            if (arr.get(i) == k) {
+                ans = i;
+            }
+        }
+        return ans;
+    }
 
+}
