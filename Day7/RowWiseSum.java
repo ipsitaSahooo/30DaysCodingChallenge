0f3efb6d8d59aef004ec9385cba6df78274f3b0a
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-04-03 23:44:36 +0530

    Update RowWiseSum.java

--- Day7/RowWiseSum.java
+++ Day7/RowWiseSum.java
@@ -1,1 +1,25 @@
+public class RowWiseSum 
+{
+	public static void rowSum(int a[][],int row,int col)
+	{
+		for(int i=0;i<row;i++)
+		{
+			int sum=0;
+			for(int j=0;j<col;j++)
+			{
+				sum+=a[i][j];
+			}
+			System.out.print(sum+" ");
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) 
+	{
+		int a[][]= {{1,2,3},{4,5,6},{7,8,9}};
+		rowSum(a,3,3);
+
+	}
+
+}
 
