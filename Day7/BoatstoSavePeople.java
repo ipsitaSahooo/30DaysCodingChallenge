d372e65b3a2d42494796c08a9577e55d8910b436
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-04-03 22:23:43 +0530

    Update BoatstoSavePeople.java

--- Day7/BoatstoSavePeople.java
+++ Day7/BoatstoSavePeople.java
@@ -1,1 +1,29 @@
-
+class Solution {
+    public int numRescueBoats(int[] ar, int limit) 
+    {
+        Arrays.sort(ar);
+        int n=ar.length;
+        int s=0;
+        int e=n-1;
+        int sum=0;
+        int boat=0;
+        while(s<=e)
+        {
+            sum=ar[s]+ar[e];
+            if(sum<=limit)
+            {
+                boat++;
+                e--;
+                s++;
+            }
+            else
+            {
+                boat++;
+                e--;
+            }
+        }
+        return boat;
+        
+      
+    }
+}
