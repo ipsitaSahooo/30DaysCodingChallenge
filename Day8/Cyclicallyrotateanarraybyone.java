36ee362183399a1aba9463615b8845914ab4cd92
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-04-04 19:45:29 +0530

    Update Cyclicallyrotateanarraybyone.java

--- Day8/Cyclicallyrotateanarraybyone.java
+++ Day8/Cyclicallyrotateanarraybyone.java
@@ -1,1 +1,19 @@
-
+class Compute 
+{
+    public void reverse(int a[],int i,int j)
+    {
+        while(i<j)
+        {
+        int t=a[i];
+        a[i]=a[j];
+        a[j]=t;
+        i++;
+        j--;
+        }
+    }
+    public void rotate(int arr[], int n)
+    {
+        reverse(arr,0,n-1);
+        reverse(arr,1,n-1);
+    }
+}
