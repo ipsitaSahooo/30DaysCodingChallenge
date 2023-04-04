5b3419a7e9d10f85a753e07468e1ff089f39f897
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-04-04 20:04:58 +0530

    Update LargestRowSum(2DArray).java

--- Day8/LargestRowSum(2DArray).java
+++ Day8/LargestRowSum(2DArray).java
@@ -1,1 +1,31 @@
+public class LargestRowSum 
+{
+	public static void MaxColSum(int a[][],int row,int col)
+	{
+		int max=Integer.MIN_VALUE;
+		int rowindex=-1;
+		for(int i=0;i<row;i++)
+		{
+			int sum=0;
+			for(int j=0;j<col;j++)
+			{
+				sum+=a[j][i];
+			}
+			if(sum>max)
+			{
+				max=sum;
+				rowindex=i;
+			}
+		}
+		System.out.println(max+" "+rowindex);
+		
+	}
+
+	public static void main(String[] args) 
+	{
+		int a[][]= {{1,2,3},{4,5,6},{7,8,9}};
+		MaxColSum(a,3,3);
+
+	}
+}
 
