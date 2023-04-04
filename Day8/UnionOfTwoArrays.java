0c7bb1f418d6799cbf285961f295676c95bf3262
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-04-04 19:32:07 +0530

    Update UnionOfTwoArrays.java

--- Day8/UnionOfTwoArrays.java
+++ Day8/UnionOfTwoArrays.java
@@ -1,1 +1,22 @@
-
+class Solution{
+    public static int doUnion(int a[], int n, int b[], int m) 
+    {
+        int c=0;
+        int arr[]=new int[n+m];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=a[i];
+        }
+        for(int i=0;i<m;i++)
+        {
+            arr[i+n]=b[i];
+        }
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length-1;i++)
+        {
+            if(arr[i]!=arr[i+1])
+            c++;
+        }
+        return c+1;
+    }
+}
