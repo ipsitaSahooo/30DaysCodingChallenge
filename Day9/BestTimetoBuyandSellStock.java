d3749c5754b988d9f2ea175a29b92d9782c83e8e
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-04-05 22:50:11 +0530

    Update BestTimetoBuyandSellStock.java

--- Day9/BestTimetoBuyandSellStock.java
+++ Day9/BestTimetoBuyandSellStock.java
@@ -1,1 +1,21 @@
+class Solution {
+    public int maxProfit(int[] a) 
+    {
+        int min=Integer.MAX_VALUE;
+        int ans=0;
+        int profit=0;
+        
 
+        for(int i=0;i<a.length;i++)
+        {
+            if(a[i]<min)
+            min= a[i];
+
+            profit=a[i]-min;
+            if(ans<profit)
+            ans=profit;
+        }
+        return ans;
+
+    }
+}
