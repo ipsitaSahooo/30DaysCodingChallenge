6c70ed2272a7b802cbf040a903a81b0de6afb344
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-04-06 22:44:58 +0530

    Update Maxsumintheconfiguration.java

--- Day10/Maxsumintheconfiguration.java
+++ Day10/Maxsumintheconfiguration.java
@@ -1,1 +1,24 @@
+class GfG
+{
+    
+    int max_sum(int a[], int n)
+    {
+       int sum=0;
+       int val=0;
+       for(int i=0;i<a.length;i++)
+       {
+           sum+=a[i];
+           val+=i*a[i];
+       }
+       int ans=val;
+       for(int i=1;i<a.length;i++)
+       {
+           int nextval= val-(sum-a[i-1])+(a[i-1]*(n-1));
+           val=nextval;
+           ans=Math.max(ans,nextval);
+           
+       }
+       return ans;
+    }	
+}
 
