3706b8b9f912dd1ec9c5e0b2679ca733a6372523
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-04-07 22:46:11 +0530

    Update Rotate2DMatrixBy90Degree.java

--- Day11/Rotate2DMatrixBy90Degree.java
+++ Day11/Rotate2DMatrixBy90Degree.java
@@ -1,1 +1,26 @@
+class Solution {
+    public void rotate(int[][] matrix) 
+    {
+        //transpose the matrix
+        for(int i=0;i<matrix[0].length;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
 
+        //reverse the matrix
+        for(int i=0;i<matrix[0].length;i++)
+        {
+            for(int j=0;j<matrix[0].length/2;j++)
+            {
+                int t=matrix[i][j];
+                matrix[i][j]=matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j]=t;
+            }
+        }
+    }
+}
