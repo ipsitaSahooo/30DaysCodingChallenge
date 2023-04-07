e385eddb5d2b81cfadbf9197e9ac6ebbb01017d5
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-04-07 23:37:05 +0530

    Update Searcha2DMatrixII.java

--- Day11/Searcha2DMatrixII.java
+++ Day11/Searcha2DMatrixII.java
@@ -1,1 +1,23 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) 
+    {
+        int row=matrix.length;
+        int col=matrix[0].length;
 
+        int rowIndex=0;
+        int colIndex=col-1;
+
+        while(rowIndex<row && colIndex>=0)
+        {
+            int n=matrix[rowIndex][colIndex];
+             if(n==target)
+             return true;
+
+             if(target > n)
+             rowIndex++;
+             else
+             colIndex--;
+        }
+        return false;
+    }
+}
