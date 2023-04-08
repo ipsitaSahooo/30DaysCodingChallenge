93d4b55e81d8a7315b934612100120e8becf3c80
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-04-08 23:44:04 +0530

    Update TwoSum(HashMap).java

--- Day12/TwoSum(HashMap).java
+++ Day12/TwoSum(HashMap).java
@@ -1,1 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
 
+class Solution {
+    public int[] twoSum(int[] nums, int target) 
+    {
+        HashMap<Integer, Integer> mk = new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(mk.containsKey(target-nums[i]))
+            return new int[] {mk.get(target-nums[i]),i};
+
+            mk.put(nums[i],i);
+        }
+        return new int[] {};
+    }
+}
