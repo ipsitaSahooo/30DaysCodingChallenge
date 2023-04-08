8f6a31a1bfcc80da47fba9d9885b23b241c77e97
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-04-08 23:23:30 +0530

    Update CountPrimes.java

--- Day12/CountPrimes.java
+++ Day12/CountPrimes.java
@@ -1,1 +1,23 @@
+class Solution {
+    public int countPrimes(int n) 
+    {
+        long c=0;
+        boolean prime[]=new boolean[n];
 
+        for(long i=2;i<n;i++)
+        {
+            if(prime[(int)i])
+            continue;
+            else
+            c++;
+
+            for(long j=i*i;j<n;j+=i)
+            {
+                prime[(int)j]=true;
+            }
+        }
+
+        return (int)c;
+       
+    }
+}
