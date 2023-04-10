3c12f11f2351ed04ecfba5ab493f19752fdf491c
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-04-10 23:40:42 +0530

    Update CatalanNumber.java

--- Day14/CatalanNumber.java
+++ Day14/CatalanNumber.java
@@ -1,1 +1,19 @@
+class Solution
+{
+    //Function to find the nth catalan number.
+    public static BigInteger findCatalan(int n)
+    {
+       int res = 0;
+ 
+        if (n <= 1) {
+            return 1;
+        }
+        for (int i = 0; i < n; i++) {
+            res += catalan(i) * catalan(n - i - 1);
+        }
+        
+        return res;
+        
+    }
+}
 
