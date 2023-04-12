2ac663ae674927b25a02a59b8508a6da1e49c05a
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-04-12 23:31:58 +0530

    Update SimplifyPath.java

--- Day16/SimplifyPath.java
+++ Day16/SimplifyPath.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    public String simplifyPath(String path) 
+    {
+        Stack<String> stack=new Stack<>();
+        final String[] str=path.split("/");
+        for(final String i:str)
+        {
+            if((i.isEmpty()) || (i.equals(".")))
+            continue;
+            if(i.equals(".."))
+            {
+            if(!stack.isEmpty())
+            stack.pop();
+            }
+            else
+            stack.push(i);
+        }
+        return "/"+String.join("/",stack);
+    }
+}
