7acb1ab6c5b0cc5e85a045f91ce0ae89bde0743d
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-04-13 22:52:21 +0530

    Update RecursiveFibonacci.java

--- Day17/RecursiveFibonacci.java
+++ Day17/RecursiveFibonacci.java
@@ -1,1 +1,12 @@
+class Solution {
+    public int fib(int n) 
+    {
+        if(n==0 || n==1)
+        return n;
 
+        int sum=0;
+        sum=fib(n-1)+fib(n-2);
+
+        return sum;
+    }
+}
