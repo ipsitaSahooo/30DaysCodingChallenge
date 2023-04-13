8e7f9dd88c9ef22f5763d6c2f64856fd883bd81b
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-04-13 23:33:53 +0530

    Update NumbersInWords.java

--- Day17/NumbersInWords.java
+++ Day17/NumbersInWords.java
@@ -1,1 +1,27 @@
+package codeStudio;
+import java.util.*;
+public class NumbersInWords 
+{
+	public static void sayDigit(int n,String arr[])
+	{
+		if(n==0)
+			return;
+		int digit=n%10;
+		n/=10;
+		
+		sayDigit(n,arr);
+		System.out.print(arr[digit]+" ");
+		
+	}
 
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		String arr[]= {"zero","one","two","three","four","five","six","seven","eight","nine"};
+		int n=s.nextInt();
+		
+		sayDigit(n,arr);
+		
+	}
+
+}
