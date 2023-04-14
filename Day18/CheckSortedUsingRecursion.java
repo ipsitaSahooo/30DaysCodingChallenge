30be805e801031377f399c2719fe929035b02adc
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-04-14 22:57:07 +0530

    Update CheckSortedUsingRecursion.java

--- Day18/CheckSortedUsingRecursion.java
+++ Day18/CheckSortedUsingRecursion.java
@@ -1,1 +1,25 @@
+public class CheckSortedUsingRecursion 
+{
+	public static boolean sorted(int a[],int n)
+	{
+		boolean ans;
+		if(n==0 || n==1)
+			return true;
+		
+		if(a[n-2]>a[n-1])
+			return false;
+		else
+			 ans=sorted(a,n-1);
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a[]= {3,4,7,5,2};
+		System.out.println(sorted(a,5));
+
+	}
+
+}
 
