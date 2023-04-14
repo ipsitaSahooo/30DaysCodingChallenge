cc2dbe6266b9e2ac674a1161fce6d04dc0993f6d
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-04-14 23:34:30 +0530

    Update LinearSearchInRecursion.java

--- Day18/LinearSearchInRecursion.java
+++ Day18/LinearSearchInRecursion.java
@@ -1,1 +1,26 @@
+public class LinearSearchInRecursion 
+{
+	public static boolean search(int a[],int n,int key)
+	{
+		boolean ans;
+		if(n==0)
+			return false;
+		
+		if(key==a[n-1])
+			return true;
+		else
+			ans=search(a,n-1,key);
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a[]= {3,4,7,5,2};
+		System.out.println(search(a,5,9));
+
+
+	}
+
+}
 
