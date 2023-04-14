1cad2d4bf8efbac7510f920df391f591c179d5cd
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-04-14 23:25:55 +0530

    Update SumOfArrayInRecursion.java

--- Day18/SumOfArrayInRecursion.java
+++ Day18/SumOfArrayInRecursion.java
@@ -1,1 +1,28 @@
+public class SumOfArrayInRecursion 
+{
+	static int sum=0;
+	public static int sumofarray(int a[],int n)
+	{
+		
+		if(n==0)
+		return 0;
+		
+		else
+		sum+=a[n-1];
+		
+		sumofarray(a,n-1);
+		return sum;
+		 
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a[]= {3,4,7,5,2};
+		System.out.println(sumofarray(a,5));
+
+
+	}
+
+}
 
