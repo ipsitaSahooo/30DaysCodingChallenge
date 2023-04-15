7f775b56de97efd4f9b71d995118aa91b4298f2e
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-04-15 23:43:51 +0530

    Update ReverseStringUsingRecursion.java

--- Day19/ReverseStringUsingRecursion.java
+++ Day19/ReverseStringUsingRecursion.java
@@ -1,1 +1,27 @@
+public class ReverseStringUsingRecursion 
+{
+	static String str="";
+	public static String reverse(String s,int n)
+	{
+		
+		if(n==0)
+			return "";
+		else
+		{
+			str+=s.charAt(n-1);
+			reverse(s,n-1);
+		}
+		
+		return str;
+	}
+
+	public static void main(String[] args) 
+	{
+		String s="ipsita";
+		System.out.println(reverse(s,s.length()));
+		
+
+	}
+
+}
 
