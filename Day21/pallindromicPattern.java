e781aa0c97b08408e988ef5049f8f928898f931e
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-04-17 22:39:20 +0530

    Update pallindromicPattern.java

--- Day21/pallindromicPattern.java
+++ Day21/pallindromicPattern.java
@@ -1,1 +1,27 @@
+public class pallindromicPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=1;i<=5;i++)
+		{
+			for(int s=1;s<=(5-i);s++)
+			{
+				System.out.print(" ");
+			}
+			//first half
+			for(int j=i;j>=1;j--)
+			{
+				System.out.print(j);
+			}
+			for(int j=2;j<=i;j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
 
