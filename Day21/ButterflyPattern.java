c41be38d1c3415ed0edd93e1852e0e93156f01d4
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-04-17 22:37:54 +0530

    Update ButterflyPattern.java

--- Day21/ButterflyPattern.java
+++ Day21/ButterflyPattern.java
@@ -1,1 +1,38 @@
+public class butterflyPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//upper half
+		for(int i=1;i<=4;i++){
+			for(int j=1;j<=i;j++){
+				System.out.print("*");
+			}
+			
+			for(int s=1;s<=(4-i)*2;s++){
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		//lower half
+		for(int i=4;i>=1;i--){
+			for(int j=1;j<=i;j++){
+				System.out.print("*");
+			}
+			
+			for(int s=1;s<=(4-i)*2;s++){
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
 
